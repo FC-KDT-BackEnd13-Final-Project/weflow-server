2baaecd5e99b1e7f918a712230833d02a457118f
shawnchoi8 <bychoise8@gmail.com>
2025-11-25 16:29:05 +0900

    feat: add Log Entity

---
+++ src/main/java/com/rdc/weflow_server/entity/log/ActivityLog.java
@@ -0,0 +1,43 @@
+package com.rdc.weflow_server.entity.log;
+
+import com.rdc.weflow_server.entity.BaseEntity;
+import com.rdc.weflow_server.entity.project.Project;
+import com.rdc.weflow_server.entity.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "activity_logs")
+@Entity
+public class ActivityLog extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private ActionType actionType;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private TargetTable targetTable;
+
+    @Column
+    private Long targetId;
+
+    @Column
+    private String ipAddress;
+
+    @OneToMany(mappedBy = "activityLog")
+    private List<User> users;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+}

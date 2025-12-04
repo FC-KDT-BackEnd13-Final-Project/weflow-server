6fe64493df013c347fbfbd821f0b6d409119823b
shawnchoi8 <bychoise8@gmail.com>
2025-12-04 14:20:57 +0900

    refactor: remove redundant ResponseEntity wrapper from PostController
    - ResponseEntity<ApiResponse<T>>를 ApiResponse<T>로 간소화
    - POST 메서드에 @ResponseStatus(HttpStatus.CREATED) 추가하여 201 상태 유지
    - 불필요한 이중 래핑 제거로 코드 일관성 개선

--- src/main/java/com/rdc/weflow_server/controller/post/PostController.java
+++ src/main/java/com/rdc/weflow_server/controller/post/PostController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -50,6 +51,7 @@ public class PostController {
      * 게시글 작성
      */
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<PostCreateResponse> createPost(
             @PathVariable Long projectId,
             @RequestBody PostCreateRequest request
@@ -87,6 +89,7 @@ public class PostController {
      * 질문에 대한 답변 등록
      */
     @PostMapping("/{postId}/questions/{questionId}/answer")
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<PostAnswerResponse> answerQuestion(
             @PathVariable Long projectId,
             @PathVariable Long postId,
